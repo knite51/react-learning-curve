44c073f8f034de6d663d37ada1230f4419ba81b1
Ayotunde <knite51@gmail.com>
2023-10-24 17:36:14 +0100

    passing params into a react query

--- Learn-intermidate/src/hooks/usePost.ts
+++ Learn-intermidate/src/hooks/usePost.ts
@@ -2,16 +2,17 @@ import axios from "axios";
 import { Post } from "../app.interface";
 import { useQuery } from '@tanstack/react-query';
 
-const usePosts = () => {
-    const fetchPosts = () => axios
-        .get('https://jsonplaceholder.typicode.com/posts')
-        .then((res) => res.data)
-
-    return useQuery<Post[], Error>({
-        queryKey: ['posts'],
-        queryFn: fetchPosts,
+const usePosts = (userId: number | undefined) =>
+    useQuery<Post[], Error>({
+        queryKey: userId ? ['posts', 'users', userId] : ['posts'],
+        queryFn: () => axios
+            .get('https://jsonplaceholder.typicode.com/posts', {
+                params: {
+                    userId
+                }
+            })
+            .then((res) => res.data),
         staleTime: 1 * 60 * 1000 //1min
     });
-}
 
 export default usePosts;
