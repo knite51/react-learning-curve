3a693a6a7f71c638d60f2d8b9f25d6fa2742f980
Ayotunde <knite51@gmail.com>
2023-10-29 15:21:38 +0000

    abstract client api with methods

--- Game-app-intermidate/src/services/api-client.ts
+++ Game-app-intermidate/src/services/api-client.ts
@@ -1,8 +1,32 @@
 import axios from "axios";
+import { GenericResponse } from "../interface/api-interface";
 
-export default axios.create({
-  baseURL: "https://api.rawg.io/api",
-  params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
-  },
+export const axiosInstance = axios.create({
+  baseURL: 'https://api.rawg.io/api',
+  params: { key: '884582f593234b8aa286642b16a30550' }
 });
+
+
+class APIClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = () => {
+    return axiosInstance.get<GenericResponse<T>>(this.endpoint).then(res => res.data);
+  }
+
+  getAllWithParams = (params: {
+    genres: number | undefined,
+    parent_platforms: number | undefined,
+    ordering: string,
+    search: string
+  }) => {
+    return axiosInstance.get<GenericResponse<T>>(this.endpoint, { params }).then(res => res.data);
+  }
+}
+
+export default APIClient;
+
