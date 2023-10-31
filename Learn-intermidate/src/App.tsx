a97283a72ee3fad9da158dabf2d7c2ec3963a5b2
Ayotunde <knite51@gmail.com>
2023-10-31 09:57:55 +0000

    custom provider for context management

--- Learn-intermidate/src/App.tsx
+++ Learn-intermidate/src/App.tsx
@@ -2,23 +2,22 @@ import { useReducer } from 'react';
 import './App.css';
 import taskReducer from './state-management/reducers/taskReducer';
 
+import AuthProvider from './state-management/AuthProvider';
 import HomePage from './state-management/HomePage';
 import NavBar from './state-management/NavBar';
-import AuthContext from './state-management/contexts/authContext';
 import TasksContext from './state-management/contexts/tasksContext';
-import authReducer from './state-management/reducers/authReducer';
 
 function App() {
   const [tasks, taskDispatch] = useReducer(taskReducer, []);
-  const [user, authDispatch] = useReducer(authReducer, '');
+
   return (
     <>
-      <AuthContext.Provider value={{ user, dispatch: authDispatch }}>
+      <AuthProvider>
         <TasksContext.Provider value={{ tasks, dispatch: taskDispatch }}>
           <NavBar></NavBar>
           <HomePage></HomePage>
         </TasksContext.Provider>
-      </AuthContext.Provider>
+      </AuthProvider>
       {/* <LoginStatus></LoginStatus> */}
       {/* <TaskList></TaskList> */}
       {/* <TodoFormOptimisticUpdate></TodoFormOptimisticUpdate> */}
