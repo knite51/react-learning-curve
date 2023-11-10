63df08ed099469c184f2b94d0f992bc40d25f80f
Ayotunde <knite51@gmail.com>
2023-11-10 19:25:01 +0000

    add expandable text

---
+++ Game-app-intermidate/src/components/ExpandableText.tsx
@@ -0,0 +1,36 @@
+import { Button, Text } from '@chakra-ui/react';
+import React, { ReactNode, useState } from 'react';
+
+interface Props {
+  children: string;
+}
+
+const ExpandableText = ({ children }: Props) => {
+  const [expanded, setExtended] = useState(false);
+  const limit = 300;
+
+  if (!children) return null;
+
+  if (children.length <= limit) return <Text></Text>;
+
+  const summary = expanded ? children : children.substring(0, limit) + '...';
+
+  return (
+    <>
+      <Text>
+        {summary}
+        <Button
+          size={'xs'}
+          fontWeight={'bold'}
+          marginLeft={1}
+          colorScheme="yellow"
+          onClick={() => setExtended(!expanded)}
+        >
+          {expanded ? 'Show Less' : 'Show More'}
+        </Button>
+      </Text>
+    </>
+  );
+};
+
+export default ExpandableText;
